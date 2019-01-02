2b4d34c782fb02e2426c9a0d5c2ea1274312e379
Yusuke Sakurai <kerokerokerop@gmail.com>
2019-01-02 16:42:27 +0900

    fix: pubsub tests

--- redis_test.ts
+++ redis_test.ts
@@ -1,11 +1,16 @@
 import {connect} from "./redis.ts";
 import {test, assertEqual, setFilter} from "https://deno.land/x/testing/testing.ts"
 import {args} from "deno";
+import {RedisPubSubMessage} from "./pubsub.ts";
 
 if (args.length > 1) {
     setFilter(args[1])
 }
-
+async function wait(duration) {
+    return new Promise(resolve => {
+        setTimeout(resolve, duration)
+    });
+}
 // can be substituted with env variable
 const addr = "127.0.0.1:6379";
 
@@ -96,4 +101,65 @@ test(async function testDecrby() {
     assertEqual(rep, -101);
     assertEqual(await redis.get("decryby"), "-101");
     redis.close();
+});
+
+test(async function testSubscribe() {
+    const redis = await connect(addr);
+    const sub = await redis.subscribe("subsc");
+    //const hoge = await redis.get("hoge");
+    const unsub = await sub.unsubscribe("subsc");
+    await sub.close();
+    assertEqual(sub.isClosed, true);
+    redis.close()
+});
+
+test(async function testSubscribe2() {
+    const redis = await connect(addr);
+    const pub = await connect(addr);
+    const sub = await redis.subscribe("subsc2");
+    let message: RedisPubSubMessage;
+    (async function () {
+        const it = sub.receive();
+        message = (await it.next()).value;
+    })();
+    await pub.publish("subsc2", "wayway");
+    await sub.close();
+    await wait(100);
+    assertEqual(message, {
+        channel: "subsc2",
+        message: "wayway"
+    });
+    const a = await redis.get("aaa");
+    assertEqual(a, void 0);
+    pub.close();
+    redis.close()
+});
+
+test(async function testPsubscribe() {
+    const redis = await connect(addr);
+    const pub = await connect(addr);
+    const sub = await redis.psubscribe("ps*");
+    let message1;
+    let message2;
+    (async function () {
+        const it = sub.receive();
+        message1 = (await it.next()).value;
+        message2 = (await it.next()).value;
+    })();
+    await pub.publish("psub", "wayway");
+    await pub.publish("psubs", "heyhey");
+    await sub.close();
+    await wait(100);
+    assertEqual(message1, {
+        pattern: "ps*",
+        channel: "psub",
+        message: "wayway"
+    });
+    assertEqual(message2, {
+        pattern: "ps*",
+        channel: "psubs",
+        message: "heyhey"
+    });
+    pub.close();
+    redis.close()
 });
