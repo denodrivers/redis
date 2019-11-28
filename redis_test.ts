c6125df867a994e2811636bae82ab0dc0866c9a8
Suguru Motegi <suguru.motegi@gmail.com>
2019-11-28 00:31:18 -0800

    test: add db 0 test case

--- redis_test.ts
+++ redis_test.ts
@@ -128,11 +128,24 @@ test(async function testConcurrent() {
   assertEquals(c, "c");
 });
 
-test(async function testDbOption() {
-  await redis.set("exists", "aaa");
-  const client = await connect({ ...addr, db: 1 });
-  const exists = await client.exists("exists");
-  assertEquals(exists, 0);
+test(async function testDb0Option() {
+  const key = "exists";
+  await redis.set(key, "aaa");
+  const exists1 = await redis.exists(key);
+  assertEquals(exists1, 1);
+  const client2 = await connect({ ...addr, db: 0 });
+  const exists2 = await client2.exists(key);
+  assertEquals(exists2, 1);
+});
+
+test(async function testDb1Option() {
+  const key = "exists";
+  await redis.set(key, "aaa");
+  const exists1 = await redis.exists(key);
+  assertEquals(exists1, 1);
+  const client2 = await connect({ ...addr, db: 1 });
+  const exists2 = await client2.exists(key);
+  assertEquals(exists2, 0);
 });
 
 [Infinity, NaN, "", "port"].forEach(v => {
