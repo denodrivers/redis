87074d4d4fc7b7a6d74c8cdf364378296ba799c5
Tuan Le <tuanleminh1235@gmail.com>
2020-07-09 13:54:37 -0400

    Cleanup test_util.ts

--- tests/test_util.ts
+++ tests/test_util.ts
@@ -1,6 +1,5 @@
 import { connect, Redis, RedisConnectOptions } from "../redis.ts";
 import { delay } from "../vendor/https/deno.land/std/async/mod.ts";
-import { assert } from "../vendor/https/deno.land/std/testing/asserts.ts";
 
 type TestFunc = () => void | Promise<void>;
 type TestServer = {
@@ -100,64 +99,4 @@ async function exists(path: string): Promise<boolean> {
     }
     throw err;
   }
-}
-
-function* dbIndex() {
-  let i = 0;
-  while (true) {
-    yield i;
-    i++;
-    if (i > 15) i = 0;
-  }
-}
-
-const it = dbIndex();
-
-function db(): number {
-  const { value } = it.next();
-  assert(value !== undefined);
-  return value;
-}
-
-export async function makeTest(
-  prefix: string,
-): Promise<{
-  client: Redis;
-  opts: RedisConnectOptions;
-  test: (name: string, func: () => void | Promise<void>) => void;
-}> {
-  const opts = { hostname: "127.0.0.1", port: 6379, db: db() };
-  const client = await connect(opts);
-  async function beforeEach() {
-    await client.flushdb(false);
-  }
-  const test = (desc: string, func: () => void | Promise<void>) => {
-    Deno.test(`[${prefix}] ${desc}`, async () => {
-      await beforeEach();
-      await func();
-    });
-  };
-  return { test, client, opts };
-}
-
-interface StartRedisServerOptions {
-  port: number;
-}
-
-export async function startRedisServer(
-  options: StartRedisServerOptions,
-): Promise<Deno.Process> {
-  const { port } = options;
-  const process = Deno.run({
-    cmd: ["redis-server", "--port", port.toString()],
-    stdin: "null",
-    stdout: "null",
-  });
-  await waitForPort(port);
-  return process;
-}
-
-// FIXME!
-function waitForPort(_port: number): Promise<void> {
-  return delay(500);
 }
