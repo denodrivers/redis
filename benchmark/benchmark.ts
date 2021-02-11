b52982daf7f2dc167d07874571c5cea3952cbed6
uki00a <uki00a@gmail.com>
2021-02-11 20:29:59 +0900

    ci: Add benchmarks

---
+++ benchmark/benchmark.ts
@@ -0,0 +1,38 @@
+import { bench, runBenchmarks } from "../vendor/https/deno.land/std/testing/bench.ts"
+
+interface Redis {
+  ping(message: string): Promise<string>;
+}
+
+interface RunOptions {
+  driver: string;
+  client: Redis;
+}
+
+export async function run(options: RunOptions): Promise<void> {
+  const {
+    driver,
+    client,
+  } = options;
+
+  bench({
+    name: `${driver}: ping`,
+    runs: 1000,
+    func: async (b) => {
+      b.start();
+      await client.ping("HELLO");
+      b.stop();
+    },
+  });
+
+  const result = await runBenchmarks();
+  const results = result.results.map(({ name, totalMs, runsCount, measuredRunsAvgMs }) => {
+    return {
+      name,
+      totalMs,
+      runsCount,
+      measuredRunsAvgMs,
+    };
+  });
+  console.table(results);
+}
---
+++ benchmark/ioredis.js
@@ -0,0 +1,16 @@
+import { run } from "./benchmark.js";
+import { performance } from "perf_hooks";
+import Redis from "ioredis";
+
+global.performance = performance;
+global.Deno = {};
+
+const redis = new Redis();
+redis.on("connect", async () => {
+  await run({
+    client: redis,
+    prefix: "ioredis",
+  });
+
+  redis.disconnect();
+});
