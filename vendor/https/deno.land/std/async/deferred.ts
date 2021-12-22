00fa4fd8c0df382119db844da5b809e5c7431f2f
Yuki Tanaka <uki00a@gmail.com>
2021-12-23 03:19:57 +0900

    bump: deno@v1.17.0 (#282)

--- vendor/https/deno.land/std/async/deferred.ts
+++ vendor/https/deno.land/std/async/deferred.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.114.0/async/deferred.ts";
+export * from "https://deno.land/std@0.118.0/async/deferred.ts";
--- vendor/https/deno.land/std/testing/bench.ts
+++ vendor/https/deno.land/std/testing/bench.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.114.0/testing/bench.ts";
+export * from "https://deno.land/std@0.118.0/testing/bench.ts";
