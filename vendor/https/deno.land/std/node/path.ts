8b797aa4c5866266e8a8d58262b6d607ec63607d
uki00a <uki00a@gmail.com>
2022-10-03 09:11:51 +0900

    chore: `deno fmt`

--- vendor/https/deno.land/std/node/path.ts
+++ vendor/https/deno.land/std/node/path.ts
@@ -1,3 +1,3 @@
 export * from "https://deno.land/std@0.158.0/node/path.ts";
-import {default as dew} from "https://deno.land/std@0.158.0/node/path.ts";
+import { default as dew } from "https://deno.land/std@0.158.0/node/path.ts";
 export default dew;
