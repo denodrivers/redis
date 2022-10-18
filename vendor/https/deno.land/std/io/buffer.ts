199aa6fc3bc2c342f23febb4e28a13e85fe890be
Yuki Tanaka <uki00a@gmail.com>
2022-10-19 04:34:12 +0900

    deps: `deno@v1.26.2`, `deno_std@v0.160.0` (#339)

--- vendor/https/deno.land/std/io/buffer.ts
+++ vendor/https/deno.land/std/io/buffer.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.158.0/io/buffer.ts";
+export * from "https://deno.land/std@0.160.0/io/buffer.ts";
--- vendor/https/deno.land/std/io/util.ts
+++
@@ -1,1 +0,0 @@
-export * from "https://deno.land/std@0.158.0/io/util.ts";
