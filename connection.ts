2ca99d7ec03fdb1fe090a88a4f080047fa2d8a04
uki00a <uki00a@gmail.com>
2022-12-17 07:24:47 +0900

    Revert "chore: avoid using try-catch"
    This reverts commit a2135509ec65833ef08c4c019c92ed6fffd7008b.

--- connection.ts
+++ connection.ts
@@ -104,42 +104,48 @@ export class RedisConnection implements Connection {
     await this.sendCommand("SELECT", db);
   }
 
-  sendCommand(
+  async sendCommand(
     command: string,
     ...args: Array<RedisValue>
   ): Promise<RedisReply> {
-    return sendCommand(this.writer, this.reader, command, args).catch(
-      async (error) => {
-        if (
-          !isRetriableError(error) ||
-          this.isManuallyClosedByUser()
-        ) {
-          throw error;
-        }
-
-        for (let i = 0; i < this.maxRetryCount; i++) {
-          // Try to reconnect to the server and retry the command
-          this.close();
-          try {
-            await this.connect();
-
-            const reply = await sendCommand(
-              this.writer,
-              this.reader,
-              command,
-              args,
-            );
-
-            return reply;
-          } catch { // TODO: use `AggregateError`?
-            const backoff = this.backoff(i);
-            await delay(backoff);
-          }
-        }
-
+    try {
+      const reply = await sendCommand(
+        this.writer,
+        this.reader,
+        command,
+        args,
+      );
+      return reply;
+    } catch (error) {
+      if (
+        !isRetriableError(error) ||
+        this.isManuallyClosedByUser()
+      ) {
         throw error;
-      },
-    );
+      }
+
+      for (let i = 0; i < this.maxRetryCount; i++) {
+        // Try to reconnect to the server and retry the command
+        this.close();
+        try {
+          await this.connect();
+
+          const reply = await sendCommand(
+            this.writer,
+            this.reader,
+            command,
+            args,
+          );
+
+          return reply;
+        } catch { // TODO: use `AggregateError`?
+          const backoff = this.backoff(i);
+          await delay(backoff);
+        }
+      }
+
+      throw error;
+    }
   }
 
   /**
