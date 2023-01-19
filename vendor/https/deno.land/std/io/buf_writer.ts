7a5abb903a7df18b6f1445544e3416ffd7a1f0b8
uki00a <uki00a@gmail.com>
2023-01-20 04:14:06 +0900

    deps: `deno@v1.29.4`, `deno_std@v0.173.0`

--- vendor/https/deno.land/std/io/buf_writer.ts
+++ vendor/https/deno.land/std/io/buf_writer.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.168.0/io/buf_writer.ts";
+export * from "https://deno.land/std@0.173.0/io/buf_writer.ts";
--- vendor/https/deno.land/std/testing/bdd.ts
+++ vendor/https/deno.land/std/testing/bdd.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.168.0/testing/bdd.ts";
+export * from "https://deno.land/std@0.173.0/testing/bdd.ts";
