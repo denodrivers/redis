8193864bb7a3cd758e2a55e4801e104aca32826f
Yuki Tanaka <uki00a@gmail.com>
2023-02-07 04:01:01 +0900

    deps: `deno@v1.30.2`, `deno_std@v0.176.0` (#362)

--- vendor/https/deno.land/std/async/delay.ts
+++ vendor/https/deno.land/std/async/delay.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.173.0/async/delay.ts";
+export * from "https://deno.land/std@0.176.0/async/delay.ts";
--- vendor/https/deno.land/std/testing/asserts.ts
+++ vendor/https/deno.land/std/testing/asserts.ts
@@ -1,1 +1,1 @@
-export * from "https://deno.land/std@0.173.0/testing/asserts.ts";
+export * from "https://deno.land/std@0.176.0/testing/asserts.ts";
