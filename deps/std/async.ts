91537b8d83a44b59ac338a2d5d1669e9cd3b618e
uki00a <uki00a@gmail.com>
2024-11-04 15:10:38 +0900

    deps: upgrade `@std/{assert,async,bytes,testing}`

--- deps/std/async.ts
+++ deps/std/async.ts
@@ -1,1 +1,1 @@
-export * from "jsr:@std/async@^1.0.0/delay";
+export * from "jsr:@std/async@^1/delay";
--- deps/std/bytes.ts
+++ deps/std/bytes.ts
@@ -1,1 +1,1 @@
-export * from "jsr:@std/bytes@^1.0.0/concat";
+export * from "jsr:@std/bytes@^1/concat";
